a3d3de452cc24b21c604fb126566c2bfb645112f
MihajloESTS <milojevicm374@gmail.com>
2022-12-31 16:41:56 +0100

    fix

--- commands/play.js
+++ commands/play.js
@@ -77,7 +77,7 @@ module.exports = {
             await queue.addTracks(result.tracks)
             embed
                 .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** have been added to the Queue`)
-                .setThumbnail(playlist.thumbnail)
+                //.setThumbnail(playlist.thumbnail)
 
 		} 
         else if (interaction.options.getSubcommand() === "search") {
